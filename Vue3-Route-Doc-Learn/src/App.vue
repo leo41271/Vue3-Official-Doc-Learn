8f7e0db6417726784840f86d7b798e418e969a95
亞克 <leochiou0927@gmail.com>
2024-08-22 16:49:12 +0800

    VueRouter Essentials Named Routes : basic

--- Vue3-Route-Doc-Learn/src/App.vue
+++ Vue3-Route-Doc-Learn/src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import showExample from '@/pages/DynaParamNotFound/showExample.vue';
+import NamedRoute from '@/pages/NamedRoute.vue';
 </script>
 
 <template>
   <a href=" http://localhost:5173/">http://localhost:5173/</a>
   <hr>
-  <showExample></showExample>
-  VueRouter Essentials Routes' Matching Syntax 參考筆記詳細講解。
-  <!-- vue router path rank tester https://paths.esm.dev/?p=AAMsIPQg4AoKzidgQFoEXAmw-IEBBRYYOE0SkABTASiz1qgBpgQA1QTsFjAb3h2onsmlAmGIFsCXjXh4AIA.&t=/users/2/ -->
+  <NamedRoute></NamedRoute>
 </template>
--- Vue3-Route-Doc-Learn/src/router/index.js
+++ Vue3-Route-Doc-Learn/src/router/index.js
@@ -5,13 +5,18 @@ import AboutView from "@/views/GettingStarted/AboutView.vue"; // GettingStarted 
 import UserPost from "@/views/DynamicRouteMatching/UserPost.vue"; // NotFound 範例2
 import NotFound from "@/views/DynamicRouteMatching/NotFound.vue"; // NotFound 範例2
 import UserGeneric from "@/views/DynamicRouteMatching/UserGeneric.vue"; // NotFound 範例2
-
+import User from "@/views/NamedRoute/User.vue";
+import Home from "@/views/NamedRoute/Home.vue";
+import About from "@/views/NamedRoute/About.vue";
 const routes = [
     { path: "/", component: HomeView }, // 範例1
     { path: "/about", component: AboutView }, // 範例1
     { path: "/users/:username/posts/:postId", component: UserPost }, // 範例2 DynamicRouteParams
     { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound }, // 範例2 值在 `route.params.pathMatch` NotFoundRoute
     { path: "/user-:afterUser(.*)", component: UserGeneric }, // 範例2 值在 `route.params.afterUser` NotFoundRoute
+    { path: "/", name: "home", component: Home }, // named route範例3
+    { path: "/about", name: "about", component: About }, // named route範例3
+    { path: "/user/:username", name: "profile", component: User }, // named route範例3
 ];
 
 const router = createRouter({
