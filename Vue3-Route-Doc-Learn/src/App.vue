34bc86e8eb147abe59d84baa224cadf23f8d89d0
亞克 <leochiou0927@gmail.com>
2024-08-22 16:47:17 +0800

    VueRouter Essentials Routes' Matching Syntax : 參考筆記詳細講解。

--- Vue3-Route-Doc-Learn/src/App.vue
+++ Vue3-Route-Doc-Learn/src/App.vue
@@ -6,4 +6,6 @@ import showExample from '@/pages/DynaParamNotFound/showExample.vue';
   <a href=" http://localhost:5173/">http://localhost:5173/</a>
   <hr>
   <showExample></showExample>
+  VueRouter Essentials Routes' Matching Syntax 參考筆記詳細講解。
+  <!-- vue router path rank tester https://paths.esm.dev/?p=AAMsIPQg4AoKzidgQFoEXAmw-IEBBRYYOE0SkABTASiz1qgBpgQA1QTsFjAb3h2onsmlAmGIFsCXjXh4AIA.&t=/users/2/ -->
 </template>
