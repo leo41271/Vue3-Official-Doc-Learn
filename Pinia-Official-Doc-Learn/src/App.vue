91994bc2053b981f83d4deb4420e8896a22737b9
亞克 <leochiou0927@gmail.com>
2024-09-05 13:54:13 +0800

    Pinia Introduction : BasicExample

--- Pinia-Official-Doc-Learn/src/App.vue
+++ Pinia-Official-Doc-Learn/src/App.vue
@@ -1,10 +1,6 @@
 <script setup>
-import { useCounterStore } from '@/stores/counter'
-const counter = useCounterStore()
-counter.count++
-counter.$patch({ count: counter.count + 1 }) // 自動補全！ ✨
-counter.increment() // 或使用 action 代替
+import BasicExample from './pages/BasicExample.vue';
 </script>
 <template>
-  <div>Current Count: {{ counter.count }}</div><!-- 直接從 store 中訪問 state -->
+  <BasicExample />
 </template>
---
+++ Pinia-Official-Doc-Learn/src/components/BasicExample.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore()
+counter.count++
+counter.$patch({ count: counter.count + 1 }) // 自動補全！ ✨
+counter.increment() // 或使用 action 代替
+</script>
+<template>
+    <div>Current Count: {{ counter.count }}</div><!-- 直接從 store 中訪問 state -->
+</template>
